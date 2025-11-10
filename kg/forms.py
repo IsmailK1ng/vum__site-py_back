4c958c44533ec9424d126b05ba54a3ede3c4d9ae
Abrolu <abralov.umar00@gmail.com>
2025-11-10 11:47:27 +0500

    fix bug ver 2

--- kg/forms.py
+++ kg/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from django.core.files.base import ContentFile
+from django.conf import settings
 from .models import VehicleCardSpec, IconTemplate
 import uuid
 import os
+import shutil
 
 
 class VehicleCardSpecForm(forms.ModelForm):
@@ -24,15 +26,27 @@ class VehicleCardSpecForm(forms.ModelForm):
             try:
                 template = IconTemplate.objects.get(id=template_id)
                 
+                # Удаляем старую иконку
                 if instance.pk and instance.icon:
                     instance.icon.delete(save=False)
                 
+                # Генерируем имя файла
                 filename = f"spec_{uuid.uuid4().hex[:8]}_{os.path.basename(template.icon.name)}"
                 
-                with template.icon.open('rb') as icon_file:
-                    instance.icon.save(filename, ContentFile(icon_file.read()), save=False)
+                
+                source_path = template.icon.path
+                dest_path = os.path.join(settings.MEDIA_ROOT, 'kg_vehicles/card_icons', filename)
+                
+                # Создаём папку если нет
+                os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+                
+                # Копируем файл
+                shutil.copy2(source_path, dest_path)
+                
+                # Сохраняем путь в БД
+                instance.icon.name = f'kg_vehicles/card_icons/{filename}'
                         
-            except:
+            except Exception as e:
                 pass
         
         if commit:
