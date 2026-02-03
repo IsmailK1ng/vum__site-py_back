96be7208e8026b4ebf734b5ab8774052528a0d87
inteuz <inteuz@gmail.com>
2026-02-03 21:38:52 +0500

    X_FRAME_OPTIONS

--- myproject/settings.py
+++ myproject/settings.py
@@ -169,7 +169,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'myproject.middleware.RefreshUserPermissionsMiddleware', 
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'reversion.middleware.RevisionMiddleware',
 ]
 ROOT_URLCONF = 'myproject.urls'
